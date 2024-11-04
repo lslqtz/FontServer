815cb855165d391db0342634d3dcc5e5ec108304
lslqtz <admin@acgn.xyz>
2024-11-04 23:20:19 +0800

    Bug fix

--- subtitle.php
+++ subtitle.php
@@ -12,7 +12,7 @@ $filename = 'Content.ass';
 $filehash = 'Unknown';
 $t = time();
 $sign = GenerateSign($loginPolicy[0], $loginPolicy[1], 0, $t, $filename, $filehash);
-HTMLStart('', GetUserBar($loginPolicy[0], $loginPolicy[1], $loginPolicy[2]['AllowLogout']));
+HTMLStart('Subtitle', GetUserBar($loginPolicy[0], $loginPolicy[1], $loginPolicy[2]['AllowLogout']));
 echo "<script src=\"base64.js\"></script>\n";
 	echo "<script>function Convert() { let content = document.getElementById('file').value; if (content === null || content === '') { alert('坏内容!'); return false; } document.forms[0].children[0].value = Base64.encode(content); return true; }</script>\n";
 echo "<label for=\"file\">ASS 字幕内容:</label>\n<br>\n";
